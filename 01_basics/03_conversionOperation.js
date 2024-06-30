5afcd7e3404e52fffd2c935cf35d378582ee2fb5
saurabh kumar <120583057+saurabhkumar09@users.noreply.github.com>
2024-06-30 11:15:36 +0000

    git.useEditorAsCommitInput

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); //do not relie on numbers in javascript
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false=> 0
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false
+// "" => false
+//"hitesh" => true 
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//******************************Operations **********************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
