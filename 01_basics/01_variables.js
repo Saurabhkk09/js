cd0d8a263c0b430d3295e796bfc78e23dd6692f8
saurabh kumar <120583057+saurabhkumar09@users.noreply.github.com>
2024-06-30 09:27:30 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+//accountId = 2 //not allowed
+//if in JS any value is not defined then it will show undefined like in accountState
+
+accountEmail = "hc@hc.com"
+acoountPassword = " 21212121"
+accountCity = "bengaluru"
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional  scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) we are using nodejs, not browser as it supports ...
+
+console.log(3 + 3);
+
+console.log("saurabh")
+
+
+let name ="saurabh"
+let age = 18
+let isLoggedIN = false
+let state;
+
+//nummber =>2  to power 53
+//bigInt 
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => uniqueness
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
